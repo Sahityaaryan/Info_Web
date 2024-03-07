45a824519d46ab1f1b5d0cd916bbd30dfc798a37
sahitya <sahityaaryansingh@gmail.com>
2024-03-07 21:01:22 +0530

    some changes

--- components/conferences/Conference.jsx
+++ components/conferences/Conference.jsx
@@ -14,7 +14,7 @@ export default function Conference(){
 			</div>
 
 
-                  <div>
+                <div>
 					{
 						ConferenceData.map(function(conference){
 
@@ -37,7 +37,7 @@ export default function Conference(){
 							)
 						})
 					}
-				  </div>
+				</div>
 
 			<div className="mt-10 sm:mt-15 flex justify-center">
 				<div className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300">
