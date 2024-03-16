c957fcd80df138938f9d1a21d83ef47b99fa86ef
sahitya <sahityaaryansingh@gmail.com>
2024-03-16 15:00:09 +0530

    resources page

---
+++ pages/resources/index.jsx
@@ -0,0 +1,44 @@
+
+import Carousel from "../../components/carausel/Carausel"
+import Image from "next/image"
+import awardAndRecognitions from "../../data/awardAndRecognitionData"
+import { certificates } from "../../data/certificates"
+import ShowDataPoints from "../../components/showDataPoints/ShowDataPoints"
+export default function Resources(){
+
+    console.log("certificates: ",certificates)
+    return(
+        <>
+
+
+        {/* certificate carousel */}
+        <div className="my-20">
+        <Carousel src={certificates} />
+        </div>
+
+            {/* Award and Achievements */}
+
+           <>
+
+           <section className="py-15 sm:py-20 mt-1 sm:mt-4">
+				<div className="text-center">
+					<p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light">
+						Award and Recognitions
+					</p>
+				</div>
+
+				<ShowDataPoints
+					data={awardAndRecognitions}
+				/>
+
+				
+			</section>
+           </>
+
+
+            {/* Events carousel*/}
+        
+        </>
+    )
+
+}
