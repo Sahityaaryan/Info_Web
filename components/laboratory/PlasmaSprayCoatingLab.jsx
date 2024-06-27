f21c775e180c8e3c9186937c602387789048d7ea
SHREYASH-KUMAR1601 <shreyashkumar1601@gmail.com>
2024-06-27 15:13:26 +0530

    Changes to be committed: 	new file:   components/laboratory/HeatTreatmentLab.jsx 	new file:   components/laboratory/LabButtons.jsx 	new file:   components/laboratory/LabContent.jsx 	new file:   components/laboratory/MechanicalTestingLab.jsx 	new file:   components/laboratory/MetallographicAndCorrosionLab.jsx 	new file:   components/laboratory/NanoIndentationLab.jsx 	new file:   components/laboratory/PlasmaSprayCoatingLab.jsx 	modified:   components/shared/AppHeader.jsx 	new file:   data/laboratoryData.js 	modified:   package-lock.json 	new file:   pages/laboratory/index.jsx

---
+++ components/laboratory/PlasmaSprayCoatingLab.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import laboratoryData from '../../data/laboratoryData';
+
+
+function PlasmaSprayCoatingLab() {
+    const currentLab = laboratoryData['Plasma Spray Coating Lab'];
+
+    return (
+        <div className="lab-content-container p-8">
+            <h2 className="text-3xl font-semibold mb-8 mt-4 uppercase">{currentLab.title}</h2>
+            <p className="mb-4">{currentLab.description}</p>
+
+            <h3 className="text-xl font-semibold mb-2">
+                Our laboratory employs advanced plasma-based methodologies for the production of:
+            </h3>
+            <ul className="list-disc list-inside mb-4">
+                {currentLab.capabilities?.map((capability, index) => (
+                    <li key={index}>{capability}</li>
+                ))}
+            </ul>
+
+            <div className="border-t-2 border-black my-4 py-6"></div>
+
+           
+            {currentLab.equipment?.map((item, index) => (
+                <React.Fragment key={index}>
+                    <div className="flex flex-col md:flex-row mb-8">
+                        {/* Alternate image position based on index (odd/even) */}
+                        {index % 2 === 0 && (
+                            <>
+                                <img src={item.imageUrl} alt={item.name} className="w-full md:w-1/2 mb-4 md:mb-0 md:mr-4" />
+                                <div className="md:w-1/2">
+                                    <h4 className="text-lg font-semibold">{item.name}</h4>
+                                    <p className='text-lg '>{item.text2}</p>
+                                    <p className='text-lg '>{item.text2}</p>
+                                    {item.specs && item.specs.length > 0 && (
+                                        <p className="font-semibold">Specification:-</p>
+                                    )}
+                                    <ul className="list-disc list-inside">
+                                        {item.specs?.map((spec, i) => (
+                                            <li key={i}>{spec}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </>
+                        )}
+                        {index % 2 !== 0 && (
+                            <>
+                                <div className="md:w-1/2">
+                                    <h4 className="text-lg font-semibold">{item.name}</h4>
+                                    <p className='text-lg '>{item.text2}</p>
+                                    <p className='text-lg '>{item.text2}</p>
+                                    {item.specs && item.specs.length > 0 && (
+                                        <p className="font-semibold">Specification:-</p>
+                                    )}
+                                    <ul className="list-disc list-inside">
+                                        {item.specs?.map((spec, i) => (
+                                            <li key={i}>{spec}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                <img src={item.imageUrl} alt={item.name} className="w-full md:w-1/2 mb-4 md:mb-0 md:ml-4" />
+                            </>
+                        )}
+                    </div>
+
+                    
+                    <div className="border-t-2 border-black my-4 py-6"></div>
+                </React.Fragment>
+            ))}
+
+
+        </div>
+    );
+}
+
+export default PlasmaSprayCoatingLab;
---
+++ data/laboratoryData.js
@@ -0,0 +1,219 @@
+const laboratoryData = {
+  'Plasma Spray Coating Lab': {
+    title: 'PLASMA SPRAY COATING LABORATORY',
+    description: 'A cutting-edge technology for coating applications, utilizing ionized gases to thermally melt and propel coating materials onto substrates, ensuring strong adhesion and superior surface properties. Designed for industries requiring robust coatings in aerospace, automotive, and energy sectors, offering enhanced material performance and durability.',
+    capabilities: [
+      'Thermal Barrier coatings',
+      'Exfoliation of 2D materials',
+      'Tailoring wettability of coatings',
+      'Graphene coatings',
+      'Corrosion-resistant coatings',
+      'Wear resistant coatings',
+      'Nitride coatings',
+      'Water filtration membranes'
+    ],
+
+    equipment: [
+      {
+        name: 'Plasma Spray Gun (9MB, Oerlikon Metco, Switzerland)',
+        specs: [
+          'Maximum Power: 80 kW',
+          'Gases: Argon, Hydrogen, Helium, Nitrogen etc.'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      },
+      {
+        name: '9MC Control Unit',
+        specs: [
+          'Rotameter gas metering system',
+          'Real-time monitoring of process parameters',
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      },
+      {
+        name: 'Plasma Spray Gun (9MB, Oerlikon Metco, Switzerland)',
+        specs: [
+          'Maximum Power: 80 kW',
+          'Gases: Argon, Hydrogen, Helium, Nitrogen etc.'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      },
+      {
+        name: 'Plasma Spray Gun (9MB, Oerlikon Metco, Switzerland)',
+        specs: [
+          'Maximum Power: 80 kW',
+          'Gases: Argon, Hydrogen, Helium, Nitrogen etc.'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      },
+      {
+        name: 'Plasma Spray Gun (9MB, Oerlikon Metco, Switzerland)',
+        specs: [
+          'Maximum Power: 80 kW',
+          'Gases: Argon, Hydrogen, Helium, Nitrogen etc.'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      },
+      
+    ],
+  },
+  'Mechanical Testing Lab': {
+    title: 'MECHANICAL TESTING LABORATORY',
+     description: 'Mechanical testing lab offers comprehensive material characterization services, specializing in tensile, compression, and fatigue tests to assess material strength and durability. Catering to diverse industries, our lab aids in product development and quality control. We ensure accurate results and insights to enhance material performance, reliability, and safety.',
+    equipment: [
+      {
+        name: 'Instron 5982 UTM',
+        specs: [
+          'Max. Force: 100 kN',
+          'Accuracy: Class 0.5',
+          'Load Resolution: 40 million counts',
+          'Crosshead Travel: 1150 mm (45 in)',
+          'Crosshead Speed: 0.00005 - 1016 mm/min (0.000002 - 40 in/min)',
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      },
+      {
+        name: 'Instron 5969 UTM',
+        specs: ['Details about Instron 5969 UTM'], 
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      },
+      {
+        name: 'lorem 5982 UTM',
+        specs: [
+          'Max. Force: 100 kN',
+          'Accuracy: Class 0.5',
+          'Load Resolution: 40 million counts',
+          'Crosshead Travel: 1150 mm (45 in)',
+          'Crosshead Speed: 0.00005 - 1016 mm/min (0.000002 - 40 in/min)',
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      },{
+        name: 'Instron 5982 UTM',
+        specs: [
+          'Max. Force: 100 kN',
+          'Accuracy: Class 0.5',
+          'Load Resolution: 40 million counts',
+          'Crosshead Travel: 1150 mm (45 in)',
+          'Crosshead Speed: 0.00005 - 1016 mm/min (0.000002 - 40 in/min)',
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'The laboratory is equipped with state-of-the-art Instron machines capable of performing a wide range of tests, including tensile, compression, fatigue, and fracture toughness testing. Our team of experts is dedicated to delivering accurate and reliable results to meet your specific testing needs.',
+      }, 
+    ],
+    
+  },
+  'Metallographic and Corrosion Lab': {
+     title: 'METALLOGRAPHIC AND CORROSION LABORATORY',
+    description: 'The metallographic and corrosion laboratory provides a controlled environment for evaluating the microstructure and corrosion resistance of metals. Equipped with microscopes, image analysis software, and corrosion testing equipment, the lab enables in-depth analysis of material properties and behavior under various conditions. ',
+
+    equipment: [
+      {
+        name: 'Metallurgical Microscopes',
+        specs: [
+          'Optical Microscopes',
+          'Stereo Microscopes',
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'We specialize in material characterization, failure analysis, and corrosion testing services. Our advanced techniques and equipment offer valuable insights into material performance and enable informed decisions in research, development, and quality control.',
+      },
+      {
+        name: 'Corrosion Test Equipment',
+        specs: [
+          'Salt Spray Chambers',
+          'Electrochemical Workstations'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'We specialize in material characterization, failure analysis, and corrosion testing services. Our advanced techniques and equipment offer valuable insights into material performance and enable informed decisions in research, development, and quality control.',
+      },
+      {
+        name: 'Image Analysis Software',
+        // specs: ['Image-Pro Premier', 'MATLAB'], 
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'We specialize in material characterization, failure analysis, and corrosion testing services. Our advanced techniques and equipment offer valuable insights into material performance and enable informed decisions in research, development, and quality control.',
+      },{
+        name: 'Corrosion Test Equipment',
+        specs: [
+          'Salt Spray Chambers',
+          'Electrochemical Workstations'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'We specialize in material characterization, failure analysis, and corrosion testing services. Our advanced techniques and equipment offer valuable insights into material performance and enable informed decisions in research, development, and quality control.',
+      },
+      
+    ],
+    
+  },
+  'Nano-Indentation Lab': {
+     title: 'NANO-INDENTATION LAB',
+    description: 'The Nano-Indentation Lab is dedicated to measuring mechanical properties of materials at the nanoscale. It uses an indenter tip to apply controlled forces to a sample, providing valuable information about its hardness, modulus, and other mechanical parameters. The lab caters to research and development needs in materials science and engineering.',
+    equipment: [
+      {
+        name: 'Hysitron TI 950 TriboIndenter',
+        // specs: [
+        //   'Advanced nanoindentation and nanomechanical characterization instrument.'
+        // ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'Our facility enables accurate measurement of mechanical properties at very small scales, aiding in the development of new materials and understanding their behavior under various conditions.'
+      },
+      {
+        name: 'TTX-NHT Nanoindentation Hardness Tester',
+        specs: [
+          'High-precision instrument for nanoindentation testing.'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'Our facility enables accurate measurement of mechanical properties at very small scales, aiding in the development of new materials and understanding their behavior under various conditions.'
+      }
+    ],
+    
+  },
+  'Heat Treatment Lab': {
+     title: 'HEAT TREATMENT LAB',
+    description: 'Our Heat Treatment Lab provides comprehensive services for modifying the properties of metals through controlled heating and cooling processes. We offer a variety of heat treatment methods, including annealing, quenching, tempering, and carburizing, to enhance material strength, hardness, toughness, and wear resistance.',
+ 
+    equipment: [
+      {
+        name: 'Furnace Systems',
+        specs: [
+          'Electric Furnaces',
+          'Gas Furnaces',
+          'Vacuum Furnaces',
+          'Salt Bath Furnaces',
+          'Fluidized Bed Furnaces'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'Equipped with modern furnaces, quenching tanks, and temperature controllers, our lab is dedicated to providing precise and reliable heat treatment solutions for diverse industries. We adhere to stringent quality standards and ensure optimal results to meet the specific needs of our clients. '
+      },
+      {
+        name: 'Quenching Equipment',
+        specs: [
+          'Oil Quenching Tanks',
+          'Water Quenching Tanks',
+          'Polymer Quenching Baths',
+          'Spray Quenching Systems'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'Equipped with modern furnaces, quenching tanks, and temperature controllers, our lab is dedicated to providing precise and reliable heat treatment solutions for diverse industries. We adhere to stringent quality standards and ensure optimal results to meet the specific needs of our clients. '
+      },
+      {
+        name: 'Temperature Controllers',
+        specs: [
+          'PID Controllers',
+          'Programmable Logic Controllers (PLCs)'
+        ],
+        imageUrl: "/images/certificates/cert1.png",
+        text2: 'Equipped with modern furnaces, quenching tanks, and temperature controllers, our lab is dedicated to providing precise and reliable heat treatment solutions for diverse industries. We adhere to stringent quality standards and ensure optimal results to meet the specific needs of our clients. '
+      }
+    ],
+    
+  }
+};
+
+export default laboratoryData;
