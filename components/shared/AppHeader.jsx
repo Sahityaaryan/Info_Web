e2aef156410c2374cc6b13bfdb1c391726c4bf4c
sahitya <sahityaaryansingh@gmail.com>
2024-06-27 12:51:38 +0530

    added the publications route

--- components/shared/AppHeader.jsx
+++ components/shared/AppHeader.jsx
@@ -165,19 +165,18 @@ function AppHeader() {
 					>
 						<Link href="/sponsors">Sponsors</Link>
 					</div>
-					
-					<div>
+				
 					<div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark">
 						<Link href="/research" aria-label="Contact">
 							Research
 						</Link>
 					</div>
-					{/* <div
+					<div
 						className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
 						aria-label="About Me"
 					>
 						<Link href="/publications">Publications</Link>
-					</div>*/}
+					</div>
 
 
 					<div
@@ -233,5 +232,6 @@ function AppHeader() {
 		</motion.nav>
 	);
 }
+
 
 export default AppHeader;
