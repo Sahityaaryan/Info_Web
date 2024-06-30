1379a04e7cc0ee1a8aeebfe8b5f3cffd2d3fb5f0
iamshobh <iamshobhraj@gmail.com>
2024-07-01 01:01:29 +0530

    carousel

--- pages/publications/index.jsx
+++ pages/publications/index.jsx
@@ -1,44 +1,62 @@
+"use-client";
 
-'use-client'
-
-import React, { useState } from 'react'
-import Publications from '../../components/publications/Publications'
-import Link from 'next/link'
-import AboutCounter from '../../components/about/AboutCounter'
-
-
+import React, { useState } from "react";
+import Publications from "../../components/publications/Publications";
+import Link from "next/link";
+import AboutCounter from "../../components/about/AboutCounter";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../../components/ui/carousel";
+import { Card, CardContent } from "../../components/ui/card";
 
 export default function index() {
-
-
-    return (
-        <div>
-            <div>
-                {/* new carausel */}
-                <div
-                    style={{ width: '100vw', height: '20rem', backgroundColor: 'blue', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}
-
-                    className="my-12"
+  return (
+    <div>
+      <div>
+        {/* new carausel */}
+        <div className="mx-9 flex justify-center align-middle px-2">
+          <Carousel className="w-4/5 sm:w-5/6 self-center h-full">
+            <CarouselContent className="-ml-1">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="basis-1/2 md:basis-1/4 lg:basis-1/4 w-full h-full "
                 >
-                    <p className="text-white font-bold text-[2rem] text-center tracking-wide">New Carausel will be here</p>
-                    {/* Group photo */}
-                </div>
-                {/* <Outlet/> */}
-                {/* false useState */}
-                <div>
-
-                <Publications/>
-                </div>
-
-                <div>
-                <AboutCounter 
-				firstTitle = {{desc:"Total Snactioned Projects",value:12}}
-				secondTitle = {{desc:"Ongoing Projects",value:20}}
-				thirdTitle = {{desc:"Total Fund Recieved",value:92}}
-				fourthTitle = {{desc:"Projects Staff",value:77}}
-				/>
-                </div>
-            </div>
+                  <div className="w-full h-full">
+                    <Card className="h-full w-full">
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-2xl font-semibold">
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
-    )
+        {/* <Outlet/> */}
+        {/* false useState */}
+        <div className="mt-3">
+          <Publications />
+        </div>
+
+        <div>
+          <AboutCounter
+            firstTitle={{ desc: "Total Snactioned Projects", value: 12 }}
+            secondTitle={{ desc: "Ongoing Projects", value: 20 }}
+            thirdTitle={{ desc: "Total Fund Recieved", value: 92 }}
+            fourthTitle={{ desc: "Projects Staff", value: 77 }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,7 @@ module.exports = {
 				gray: colors.neutral,
 			},
 			container: {
+				center: true,
 				padding: {
 					DEFAULT: '1rem',
 					sm: '2rem',
@@ -33,6 +34,25 @@ module.exports = {
 					'2xl': '8rem',
 				},
 			},
+			borderRadius: {
+				lg: `var(--radius)`,
+				md: `calc(var(--radius) - 2px)`,
+				sm: "calc(var(--radius) - 4px)",
+			  },
+			  keyframes: {
+				"accordion-down": {
+				  from: { height: "0" },
+				  to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+				  from: { height: "var(--radix-accordion-content-height)" },
+				  to: { height: "0" },
+				},
+			  },
+			  animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			  },
 		},
 	},
 	plugins: [require('@tailwindcss/forms'),flowbite.plugin(),],
